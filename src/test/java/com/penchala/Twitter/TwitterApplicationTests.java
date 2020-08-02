ed3b5f3754fd118339dd14e7042aac70ba34dd0a
Siva Charan <siva3060@gmail.com>
2020-08-02 21:33:29 +0530

    PROJECT INITALISATION & INITIAL COMMIT
     Spring Starter IO and pushing to git

---
+++ src/test/java/com/penchala/Twitter/TwitterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.penchala.Twitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
