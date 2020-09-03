b9983fda3040a3437e29ca60ad5972d551f9c8ba
siva <siva3060@gmail.com>
2020-09-03 19:04:25 +0530

    HEALTH CHECK ENDPOINT RUNNING LOCALLY
    Execution of the healthcheck endpoint has been employeed
    and has been checked using CURL & Browser

--- src/main/java/com/penchala/Twitter/controller/AuthenticationHealthCheck.java
+++ src/main/java/com/penchala/Twitter/controller/AuthenticationHealthCheck.java
@@ -1,9 +1,15 @@
 
+package com.penchala.Twitter.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
 @RestController
-@RequestMapping(/application)
+@RequestMapping("/")
 public class AuthenticationHealthCheck{
 
-	@GetMapping("/HealthCheck")
+	@GetMapping("/healthcheck")
 	public String HealthCheck(){
 		return "KEEP_ALIVE_OK";
 		//end of method HealthCheck
--- src/main/java/com/penchala/Twitter/controller/UserAuthenticationController.java
+++ src/main/java/com/penchala/Twitter/controller/UserAuthenticationController.java
@@ -1,13 +1,18 @@
 
+package com.penchala.Twitter.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 @RestController 
+@RequestMapping
 public class UserAuthenticationController{
 
-	@Autowire
-	UserAuthenticationService authenticate;
 
-	public Boolean ValidateUser(String userName,String password){
-		return authenticate(userName,password)
+	public String  ValidateUser(String userName,String password){
+		//return authenticate(userName,password);
 		//end of validateUser 
+		return "IN_PROGRESS";
 	}
 	//end of class UserAuthenticationController 
 }
